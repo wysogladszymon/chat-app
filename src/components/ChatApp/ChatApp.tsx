9f4da52aa8128b8315070707c03034cce0e32ab6
Szymon <wysogladszymon@gmail.com>
2024-02-21 09:49:28 +0100

    work with inbox and message fetching done for now

--- src/components/ChatApp/ChatApp.tsx
+++ src/components/ChatApp/ChatApp.tsx
@@ -39,197 +39,339 @@ export interface inboxInterface {
   user: User;
 }
 export const ChatApp: FC<ChatAppProps> = () => {
-  const { activeState, dispatchActive } = useActiveContext();
-  const [inbox, setInbox] = useState<inboxInterface[]>([]);
-  const [friendRequests, setFriendRequests] = useState<friendRequest[]>([]);
   const { currentUser } = useAuthContext();
+  if (currentUser) {
+    const { activeState, dispatchActive } = useActiveContext();
+    const [inbox, setInbox] = useState<inboxInterface[]>([]);
+    const [friendRequests, setFriendRequests] = useState<friendRequest[]>([]);
 
-  const handleFriendReq = () => {
-    dispatchActive({ type: "FRIEND_REQUEST", payload: null });
-  };
-  const handleAddFriend = () => {
-    dispatchActive({ type: "ADD_FRIEND", payload: null });
-  };
-
-  const handledecline = async (id: string) => {
-    if (!currentUser) return;
-    const requestRef = doc(db, "friendrequests", currentUser.uid);
-
-    const users = friendRequests.filter((u) => u.uid !== id);
-    await updateDoc(requestRef, {
-      users: users,
-    });
-    setFriendRequests(users);
-  };
-
-  const handleacceptance = async (us: friendRequest) => {
-    // if (!currentUser) return;
-
-    // const combinedID = getChatID(currentUser.uid, us.uid);
-    // const docum = (await getDoc(doc(db, "chats", combinedID))).exists();
-    // // if (docum) return;
-
-    // //creates chat
-    // await setDoc(doc(db, "chats", combinedID), { messages: [] });
-    // //creates userchat
-
-    // const doc1 = await getDoc(doc(db, "userChats", currentUser.uid));
-    // const doc2 = await getDoc(doc(db, "userChats", us.uid));
-
-    // if (!doc1.exists()) 
-    //   await setDoc(doc(db, "userChats", currentUser.uid), { messages: [] });
-    // const mss = doc1.data().messages;
-    // mss.push({user: {
-    //   uid: us.uid,
-    //   email: us.email,
-    //   displayName: us.displayName,
-    //   photoURL: us.photoURL,
-    // },lastmsg: {
-    //   content: "",
-    //   date: null,
-    //   uid: "",
-    // }});
-    //   await updateDoc(doc(db, "userChats", currentUser.uid), {
-    //   messages:mss
-    // });
-
-    // if (!doc2.exists()) 
-    //   await setDoc(doc(db, "userChats", us.uid), { messages: [] });
-    //   const mss2 = doc2.data().messages;
-    //   mss2.push({user: {
-    //     uid: us.uid,
-    //     email: us.email,
-    //     displayName: us.displayName,
-    //     photoURL: us.photoURL,
-    //   },lastmsg: {
-    //     content: "",
-    //     date: null,
-    //     uid: "",
-    //   }});
-    //     await updateDoc(doc(db, "userChats", currentUser.uid), {
-    //     messages:mss
-    //   });
-    // dispatchActive({ type: "CHAT", payload: { user: us, messages: [] } });
-    // handledecline(us.uid);
-  };
-
-  const handleClick = (u: inboxInterface) => {
-    if (!currentUser) return;
-    const combinedID = getChatID(currentUser.uid, u.user.uid);
-
-    dispatchActive({ type: "CHAT", payload: { user: u.user, messages: [] } });
-  };
-  useEffect(() => {
-    const fetchFriendRequests = async () => {
-      if (!currentUser || !currentUser.uid) return;
-
-      const friendRequestsUnsub = onSnapshot(
-        doc(db, "friendrequests", currentUser.uid),
-        (doc) => {
-          setFriendRequests((doc.data()?.users as friendRequest[]) || []);
-        }
-      );
-
-      return () => friendRequestsUnsub();
+    const handleFriendReq = () => {
+      dispatchActive({ type: "FRIEND_REQUEST", payload: null });
+    };
+    const handleAddFriend = () => {
+      dispatchActive({ type: "ADD_FRIEND", payload: null });
     };
 
-    const fetchUserChats = async () => {
-      if (!currentUser || !currentUser.uid) return;
+    const handledecline = async (id: string) => {
+      const requestRef = doc(db, "friendrequests", currentUser.uid);
 
-      const userChatsUnsub = onSnapshot(
-        doc(db, "userChats", currentUser.uid),
-        (doc) => {
-          if (doc && doc.data()) {
-            console.log(Object.entries(doc.data().messages.entries));
+      //delete friendRequest
+      const users = friendRequests.filter((u) => u.uid !== id);
+      await updateDoc(requestRef, {
+        users: users,
+      });
+      setFriendRequests(users);
+    };
+
+    const handleacceptance = async (us: friendRequest) => {
+      // create chat (request cannot be send if the chat already exists)
+      const combinedID = getChatID(us.uid, currentUser.uid);
+      await setDoc(doc(db, "chats", combinedID), {
+        messages: [], //type message[]
+      });
+
+      //create userChats
+      try {
+        const doc1 = doc(db, "userChats", currentUser.uid);
+        const currUserDoc = await getDoc(doc1);
+        //if its first user chat
+        if (!currUserDoc.exists()) {
+          await setDoc(doc1, {
+            chats: [
+              {
+                user: {
+                  email: us.email,
+                  displayName: us.displayName,
+                  photoURL: us.photoURL,
+                  uid: us.uid,
+                },
+                lastmsg: {
+                  date: null,
+                  content: "",
+                  uid: "",
+                },
+              },
+            ],
+          });
+          //if user has other chats
+        } else {
+          const chats = currUserDoc.data().chats;
+          chats.push({
+            user: {
+              email: us.email,
+              displayName: us.displayName,
+              photoURL: us.photoURL,
+              uid: us.uid,
+            },
+            lastmsg: {
+              date: null,
+              content: "",
+              uid: "",
+            },
+          });
+          await updateDoc(doc1, {
+            chats: [...chats],
+          });
+        }
+        const doc2 = doc(db, "userChats", us.uid);
+        const userDoc = await getDoc(doc2);
+        //if its first user chat
+        if (!userDoc.exists()) {
+          await setDoc(doc2, {
+            chats: [
+              {
+                user: {
+                  email: currentUser.email,
+                  displayName: currentUser.displayName,
+                  photoURL: currentUser.photoURL,
+                  uid: currentUser.uid,
+                },
+                lastmsg: {
+                  date: null,
+                  content: "",
+                  uid: "",
+                },
+              },
+            ],
+          });
+          //if user has other chats
+        } else {
+          const chats2 = userDoc.data().chats;
+          chats2.push({
+            user: {
+              email: currentUser.email,
+              displayName: currentUser.displayName,
+              photoURL: currentUser.photoURL,
+              uid: currentUser.uid,
+            },
+            lastmsg: {
+              date: null,
+              content: "",
+              uid: "",
+            },
+          });
+          await updateDoc(doc2, {
+            chats: [...chats2],
+          });
+        }
+      } catch (err: any) {
+        console.error(err);
+      }
+
+      //delete user from requests
+      handledecline(us.uid);
+      dispatchActive({ type: "CHAT", payload: { user: us, messages: [] } });
+    };
+
+    const chatInfoClick = async (u: inboxInterface) => {
+      try {
+        // get messages
+        const combinedID = getChatID(currentUser.uid, u.user.uid);
+        const doc1 = await getDoc(doc(db, "chats", combinedID));
+        if (!doc1.exists()) return;
+        const mess = doc1.data().messages.map((msg: message) => {
+          return {
+            date: msg.date,
+            content: msg.content,
+            uid: msg.uid,
+          };
+        });
+        console.log(mess);
+
+        dispatchActive({
+          type: "CHAT",
+          payload: { user: u.user, messages: mess },
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    useEffect(() => {
+      const fetchFriendRequests = async () => {
+        const friendRequestsUnsub = onSnapshot(
+          doc(db, "friendrequests", currentUser.uid),
+          (doc) => {
+            const requests = doc.data()?.users as friendRequest[];
+            if (requests !== friendRequests) setFriendRequests(requests);
           }
+        );
+
+        return () => friendRequestsUnsub();
+      };
+
+      const fetchUserChats = async () => {
+        const userChatsRef = doc(db, "userChats", currentUser.uid);
+        try {
+          const userChatsUnsub = onSnapshot(
+            userChatsRef,
+            (docSnapshot) => {
+              if (docSnapshot.exists()) {
+                const chatData = docSnapshot.data();
+                const data = chatData.chats.map((el: inboxInterface) => {
+                  return {
+                    user: {
+                      email: el.user.email,
+                      displayName: el.user.displayName,
+                      photoURL: el.user.photoURL,
+                      uid: el.user.uid,
+                    },
+                    lastmsg: {
+                      content: el.lastmsg.content,
+                      date: el.lastmsg.date,
+                      uid: el.lastmsg.uid,
+                    },
+                  };
+                });
+                if (inbox !== data) setInbox(data);
+              } else {
+                console.log("No chat data available");
+              }
+            },
+            (error: any) => {
+              console.error("Error fetching user chats:", error);
+            }
+          );
+
+          return () => userChatsUnsub();
+        } catch (error) {
+          console.error("Error setting up user chats listener:", error);
         }
+      };
+      const fetchCurrentMessages = () => {
+        //if chat is not active - do nothing
+        if (!activeState.chat) return;
+
+        //get chatRef
+        const combinedID = getChatID(
+          currentUser.uid,
+          activeState.chat.user.uid
+        );
+        const chatRef = doc(db, "chats", combinedID);
+
+        try {
+          const chatUnsub = onSnapshot(
+            chatRef,
+            (docSnapshot) => {
+              if (docSnapshot.exists()) {
+                const chatData = docSnapshot.data();
+                // console.log(chatData.messages);
+
+                const data = chatData.messages.map((msg: message) => {
+                  return {
+                    date: msg.date,
+                    content: msg.content,
+                    uid: msg.uid,
+                  };
+                });
+                if (!activeState.chat || !activeState.chat.user) return;
+
+                if (activeState.chat?.messages !== data)
+                  dispatchActive({
+                    type: "CHAT",
+                    payload: { user: activeState.chat.user, messages: data },
+                  });
+              } else {
+                console.log("No chat data available");
+              }
+            },
+            (error: any) => {
+              console.error("Error fetching user chats:", error);
+            }
+          );
+
+          return () => chatUnsub();
+        } catch (error) {
+          console.error("Error setting up user chats listener:", error);
+        }
+      };
+      const DEBOUNCE_DELAY = 2000;
+      const debouncedFetchFriendRequests = debounce(
+        fetchFriendRequests,
+        DEBOUNCE_DELAY
       );
+      const debouncedFetchUserChats = debounce(fetchUserChats, DEBOUNCE_DELAY);
+      const debouncedFetchCurrMessages = debounce(
+        fetchCurrentMessages,
+        DEBOUNCE_DELAY
+      );
+      const fetchAllData = async () => {
+        await debouncedFetchFriendRequests();
+        await debouncedFetchUserChats();
+        await debouncedFetchCurrMessages();
+      };
 
-      return () => userChatsUnsub();
-    };
-    const DEBOUNCE_DELAY = 2000;
-    const debouncedFetchFriendRequests = debounce(
-      fetchFriendRequests,
-      DEBOUNCE_DELAY
-    );
-    const debouncedFetchUserChats = debounce(fetchUserChats, DEBOUNCE_DELAY);
+      currentUser?.uid && fetchAllData();
 
-    const fetchAllData = async () => {
-      await debouncedFetchFriendRequests();
-      await debouncedFetchUserChats();
-    };
+      return () => {
+        debouncedFetchFriendRequests.cancel();
+        debouncedFetchUserChats.cancel();
+      };
+    }, [currentUser?.uid, handleacceptance]);
 
-    currentUser?.uid && fetchAllData();
-
-    return () => {
-      debouncedFetchFriendRequests.cancel();
-      debouncedFetchUserChats.cancel();
-    };
-  }, [currentUser?.uid, handleacceptance]);
-
-  return (
-    <div className=" h-screen flex justify-center items-center w-screen">
-      {/* sidebar */}
-      <div className="flex flex-col border-r-2 h-full relative pt-5 max-w-[512px]">
-        <div className="flex align-center justify-end ">
-          <ToggleThemeButton />
-        </div>
-        <div className="mt-20 ">
-          <AddFriend onClick={handleAddFriend} />
-          <FriendRequests
-            onClick={handleFriendReq}
-            count={friendRequests.length}
-          />
-        </div>
-        <p className="p-4 text-gray-400 text-sm">chats</p>
-        <div className={` grow flex flex-col justify-self-end overflow-auto`}>
-          {inbox.map((u) => (
-            <ChatInfo
-              onClick={() => handleClick(u)}
-              key={u.user.uid}
-              lastmsg={u.lastmsg.content}
-              date={new Date()}
-              picURL={u.user.photoURL}
-              name={u.user.displayName}
+    return (
+      <div className=" h-screen flex justify-center items-center w-screen">
+        {/* sidebar */}
+        <div className="flex flex-col border-r-2 h-full relative pt-5 max-w-[512px]">
+          <div className="flex align-center justify-end ">
+            <ToggleThemeButton />
+          </div>
+          <div className="mt-20 ">
+            <AddFriend onClick={handleAddFriend} />
+            <FriendRequests
+              onClick={handleFriendReq}
+              count={friendRequests.length}
             />
-          ))}
-        </div>
-        <UserData />
-      </div>
-      {/* current Chat */}
-      <div className="w-full h-full bg-gray-100 flex flex-col ">
-        {activeState.chat && (
-          <Messages name={activeState.chat.user.displayName}>
-            {activeState.chat.messages.map((msg, index) => (
-              <Message
-                my={currentUser?.uid === msg.uid}
-                key={index}
-                date={new Date()}
-              >
-                {msg.content}
-              </Message>
-            ))}
-          </Messages>
-        )}
-        {activeState.addFriend && <AddFriendMenu />}
-        {activeState.friendRequest && (
-          <FriendRequestsMenu>
-            {friendRequests.map((r) => (
-              <FriendRequestUser
-                key={r.uid}
-                displayName={r.displayName}
-                email={r.email}
-                photoURL={r.photoURL}
-                decline={() => handledecline(r.uid)}
-                accept={() => handleacceptance(r)}
+          </div>
+          <p className="p-4 text-gray-400 text-sm">chats</p>
+          <div className={` grow flex flex-col justify-self-end overflow-auto`}>
+            {inbox.map((u) => (
+              <ChatInfo
+                onClick={() => chatInfoClick(u)}
+                key={u.user.uid}
+                lastmsg={u.lastmsg.content}
+                date={u.lastmsg.date}
+                picURL={u.user.photoURL}
+                name={u.user.displayName}
               />
             ))}
-          </FriendRequestsMenu>
-        )}
-        {!activeState.friendRequest &&
-          !activeState.chat &&
-          !activeState.addFriend && <Welcome />}
+          </div>
+          <UserData />
+        </div>
+        {/* current Chat */}
+        <div className="w-full h-full bg-gray-100 flex flex-col ">
+          {activeState.chat && (
+            <Messages name={activeState.chat.user.displayName}>
+              {activeState.chat.messages.map((msg, index) => (
+                <Message
+                  my={currentUser?.uid === msg.uid}
+                  key={index}
+                  date={new Date()}
+                >
+                  {msg.content}
+                </Message>
+              ))}
+            </Messages>
+          )}
+          {activeState.addFriend && <AddFriendMenu />}
+          {activeState.friendRequest && (
+            <FriendRequestsMenu>
+              {friendRequests.map((r) => (
+                <FriendRequestUser
+                  key={r.uid}
+                  displayName={r.displayName}
+                  email={r.email}
+                  photoURL={r.photoURL}
+                  decline={() => handledecline(r.uid)}
+                  accept={() => handleacceptance(r)}
+                />
+              ))}
+            </FriendRequestsMenu>
+          )}
+          {!activeState.friendRequest &&
+            !activeState.chat &&
+            !activeState.addFriend && <Welcome />}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
